1f8432d288de5f4a503b66656bfef2681ca0e176
Joey Liao <joey01271998@gmail.com>
2025-08-16 16:25:29 +0800

    Setup: 1. Modify Makefile.
    2. Add openapi.yaml.
    
    3. Modify nginx setting to fix  request redirect issue.
    
    4. Remove sample file.

--- BE/SpringBoot/src/main/java/com/penguin/linknote/repo/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.penguin.linknote.repo;
-
-import com.penguin.linknote.entity.Orders;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface OrderRepository extends CrudRepository<Orders, Long> {
-}
