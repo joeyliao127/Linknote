80a978c038f03f02be196b5f4f9126a58a8e21d5
Joey Liao <joey01271998@gmail.com>
2026-01-09 10:22:02 +0800

    refactor: remove paginateService and QDSL denpendency

--- BE/SpringBoot/src/main/java/com/penguin/linknote/common/dto/PageResponse.java
+++ BE/SpringBoot/src/main/java/com/penguin/linknote/common/dto/PageResponse.java
@@ -2,10 +2,7 @@ package com.penguin.linknote.common.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.domain.Page;
-
 import java.util.List;
-import java.util.function.Function;
 
 @Data
 @NoArgsConstructor
@@ -16,14 +13,4 @@ public class PageResponse<R> {
     private Integer totalPage;
     private List<R> items;
 
-    public static  <R, T> PageResponse<R> fromPage(Page<T> page, Function<T, R> mapper) {
-        PageResponse<R> pageResponse = new PageResponse<R>();
-
-        pageResponse.setCount(page.getNumberOfElements());
-        pageResponse.setCurrentPage(page.getNumber());
-        pageResponse.setPageSize(page.getSize());
-        pageResponse.setTotalPage(page.getTotalPages());
-        pageResponse.setItems(page.getContent().stream().map(mapper).toList());
-        return pageResponse;
-    }
 }
